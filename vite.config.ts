2543db08c9a680fe36b986c73c46f4011c188bb4
Manoj-Dhundhalva <manoj@titan.email>
2026-08-28 01:50:39 +0530

    [chore]: generate source map only on non-prod environment

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "node:path";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+  build: {
+    sourcemap: mode !== "prod",
+  },
+}));
